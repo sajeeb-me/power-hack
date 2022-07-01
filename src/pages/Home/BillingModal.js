68fa09677818140a96db71ec15b94b495b05eb60
sdshuvo98 <hellosdshuvo@gmail.com>
2022-07-01 15:45:21 +0600

    set form validation

--- src/pages/Home/BillingModal.js
+++ src/pages/Home/BillingModal.js
@@ -8,7 +8,6 @@ import { toast } from 'react-toastify';
 const BillingModal = ({ handleClose, show, id, setId }) => {
     const { register, formState: { errors }, handleSubmit, reset } = useForm();
 
-
     const onSubmit = (newBill) => {
         id ?
             (async () => {
@@ -71,36 +70,64 @@ const BillingModal = ({ handleClose, show, id, setId }) => {
                             <input
                                 type='email'
                                 className={`${errors.email && 'border border-danger'} w-100 p-1 rounded`}
-                                {...register("email", { required: true })}
+                                {...register("email", {
+                                    required: {
+                                        value: true,
+                                        message: "Email is required"
+                                    },
+                                    pattern: {
+                                        value: /\S+@\S+\.\S+/,
+                                        message: 'Provide a valid email'
+                                    }
+                                })}
                             />
-                            {
-                                errors.email?.type === 'required' &&
-                                <div className="form-text text-danger">Email is required</div>
-                            }
+                            <div className="form-text text-danger">
+                                {errors.email?.type === 'required' && <span>{errors.email.message}</span>}
+                                {errors.email?.type === 'pattern' && <span>{errors.email.message}</span>}
+                            </div>
                         </div>
                         <div className="mb-3">
                             <label htmlFor="phone" className="form-label d-block">Phone</label>
                             <input
                                 type='text'
                                 className={`${errors.phone && 'border border-danger'} w-100 p-1 rounded`}
-                                {...register("phone", { required: true })}
+                                {...register("phone", {
+                                    required: {
+                                        value: true,
+                                        message: "Phone number is required"
+                                    },
+                                    pattern: {
+                                        value: /^(?:\d{2}-\d{3}-\d{3}-\d{3}|\d{11})$/,
+                                        message: 'Provide a valid 11 digit number'
+                                    }
+                                })}
                             />
-                            {
-                                errors.phone?.type === 'required' &&
-                                <div className="form-text text-danger">Phone number is required</div>
-                            }
+                            <div className="form-text text-danger">
+                                {errors.phone?.type === 'required' && <span>{errors.phone.message}</span>}
+                                {errors.phone?.type === 'pattern' && <span>{errors.phone.message}</span>}
+                            </div>
+
                         </div>
                         <div className="mb-3">
                             <label htmlFor="amount" className="form-label d-block">Paid amount</label>
                             <input
                                 type='number'
                                 className={`${errors.amount && 'border border-danger'} w-100 p-1 rounded`}
-                                {...register("amount", { required: true })}
+                                {...register("amount", {
+                                    required: {
+                                        value: true,
+                                        message: "Paid amount is required"
+                                    },
+                                    min: {
+                                        value: 1,
+                                        message: 'Paid amount can not be less then one(1)'
+                                    }
+                                })}
                             />
-                            {
-                                errors.amount?.type === 'required' &&
-                                <div className="form-text text-danger">Paid amount is required</div>
-                            }
+                            <div className="form-text text-danger">
+                                {errors.amount?.type === 'required' && <span>{errors.amount.message}</span>}
+                                {errors.amount?.type === 'min' && <span>{errors.amount.message}</span>}
+                            </div>
                         </div>
 
 
