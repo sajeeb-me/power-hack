2499698dae4e6d421bbdd65a945b24ccf40b4c85
sdshuvo98 <hellosdshuvo@gmail.com>
2022-07-13 16:54:39 +0600

    developed login page

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,7 +1,38 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { useForm } from "react-hook-form";
+import { toast } from 'react-toastify';
 
 function Login() {
+    const navigate = useNavigate()
+    const { register, formState: { errors }, handleSubmit } = useForm();
+
+    const onSubmit = async (data) => {
+        // const email = data.email;
+        const user = data;
+        if (user) {
+            fetch('http://localhost:5000/login', {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(user),
+            })
+                .then(res => {
+                    // console.log(res);
+                    if (res.status === 500) {
+                        return toast.error('User not found')
+                    }
+                    return res.json()
+                })
+                .then(data => {
+                    // console.log(data);
+                    if (data.success) {
+                        navigate('/');
+                    }
+                })
+        }
+    };
     return (
         // <!-- Section: Design Block -->
         <section className="text-center">
@@ -27,17 +58,27 @@ function Login() {
                     <div className="row d-flex justify-content-center">
                         <div className="col-lg-8">
                             <h2 className="fw-bold mb-5">Login now</h2>
-                            <form>
+                            <form onSubmit={handleSubmit(onSubmit)}>
                                 {/* <!-- Email input --> */}
-                                <div className="form-outline mb-4">
-                                    <input type="email" id="form3Example3" className="form-control" />
-                                    <label className="form-label" htmlFor="form3Example3">Email address</label>
+                                <div className="form-outline mb-4 text-start">
+                                    <label className="form-label ps-1">Email address</label>
+                                    <input
+                                        type="email"
+                                        className="form-control"
+                                        {...register("email", { required: true })}
+                                    />
+                                    <span className='text-danger'>{errors.email?.type === 'required' && "Email address is required"}</span>
                                 </div>
 
                                 {/* <!-- Password input --> */}
-                                <div className="form-outline mb-4">
-                                    <input type="password" id="form3Example4" className="form-control" />
-                                    <label className="form-label" htmlFor="form3Example4">Password</label>
+                                <div className="form-outline mb-4 text-start">
+                                    <label className="form-label ps-1">Password</label>
+                                    <input
+                                        type="password"
+                                        className="form-control"
+                                        {...register("password", { required: true })}
+                                    />
+                                    <span className='text-danger'>{errors.password?.type === 'required' && "Password is required"}</span>
                                 </div>
 
                                 {/* <!-- Submit button --> */}
